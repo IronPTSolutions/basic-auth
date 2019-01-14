3ccc35c741f7d24ea00dc1f529c378e236c76d6d
Carlos del Prado <carlos.delprado@11paths.com>
2019-01-14 21:33:34 +0100

    feat: login form

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const authRouter = require('../controllers/auth.controller');
 
 router.get('/register', authRouter.register);
+router.post('/register', authRouter.doRegister);
+
+router.get('/login', authRouter.login);
+router.post('/login', authRouter.doLogin);
 
 module.exports = router;
